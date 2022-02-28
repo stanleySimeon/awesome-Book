cac76789280b303cff8a4bf7718ae67581f782ef
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-02-28 16:42:46 +0100

    Fix linters error

--- modules/date.js
+++
@@ -1,9 +0,0 @@
-// import DateTime from 'luxon/src/datetime.js';
-
-// const displayDate = (date) => {
-//   setInterval(() => {
-//     date.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
-//   }, 1000);
-// };
-
-// export default displayDate;
