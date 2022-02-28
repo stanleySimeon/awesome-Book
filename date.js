01ded6552ce958fcb0038fc1886dd863ed861a98
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-02-28 16:08:46 +0100

    remove node_modules

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
-import { DateTime } from './modules/luxon/src/luxon.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 
 const displayCurrentDate = (date) => {
   setInterval(() => {
